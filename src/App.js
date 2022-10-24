fb2fdeed5be2ee8f9cd84763112304acb4fdb872
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2022-10-24 12:27:44 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // https://geo.ipify.org/api/v2/country,city?apiKey=at_Q0IFJ9OzFG4wO9oY19PRxDZS2MzwR&ipAddress=8.8.8.8
-import './index.css';
 
 
 function App() {
@@ -18,26 +17,32 @@ function App() {
               <button type="submit" className='submit-btn'><svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6"/></svg></button>
             </form>
           </span>
+
         </span>
 
-        <span>
-          <span>
-            <p>IP Address</p>
-          </span>
-
-          <span>
-            <p>Location</p>
-          </span>
-
-          <span>
-            <p>Timezone UTC</p>
-          </span>
-
-          <span>
-            <p>ISP</p>
-          </span>
-        </span>
       </span>
+
+      <span className='details'>
+            <span className='details-innerspan'>
+              <p className='properties'>IP Address</p>
+              <p className='black-bold'>1921239443.34343</p>
+            </span>
+
+            <span className='details-innerspan'>
+              <p className='properties'>Location</p>
+              <p className='black-bold'>new york</p>
+            </span>
+
+            <span className='details-innerspan'>
+              <p className='properties'>Timezone UTC</p>
+              <p className='black-bold'>UTC- 500</p>
+            </span>
+
+            <span className='details-innerspan'>
+              <p className='properties'>ISP</p>
+              <p className='black-bold'>ower name</p>
+            </span>
+          </span>
 
     </div>
   );
